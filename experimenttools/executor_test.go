9bfb99c39908735c15b000f466e2b1c0aa0c9077
Weifan Zhang <weifanzh@es.aau.dk>
2022-11-20 12:04:42 +0100

    I add test cases to make sure that the clouds and apps do not change during every scheduling algorithms. This can make me at ease, because if clouds or apps change during scheduling, there will be unpredictable problems.

--- experimenttools/executor_test.go
+++ experimenttools/executor_test.go
@@ -3,7 +3,13 @@ package experimenttools
 import (
 	"fmt"
 	"github.com/KeepTheBeats/routing-algorithms/random"
+	"gogeneticwrsp/algorithms"
+	"gogeneticwrsp/model"
+	"log"
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateCloudsApps(t *testing.T) {
@@ -12,4 +18,119 @@ func TestGenerateCloudsApps(t *testing.T) {
 		numApps := random.RandomInt(3, 6)
 		GenerateApps(numApps, fmt.Sprintf("%d", i))
 	}
+}
+
+func TestNoChangeMCASGA(t *testing.T) {
+	var numCloud, numApp int = 5, 15
+	var appSuffix string = "0"
+
+	// generate clouds and apps, and write to files
+	GenerateClouds(numCloud)
+	GenerateApps(numApp, appSuffix)
+
+	// read clouds and apps from files
+	var clouds []model.Cloud
+	var apps []model.Application
+
+	clouds = ReadClouds(numCloud)
+	apps = ReadApps(numApp, appSuffix)
+	clouds = model.CloudsCopy(clouds)
+	apps = model.AppsCopy(apps)
+
+	oriClouds := model.CloudsCopy(clouds)
+	oriApps := model.AppsCopy(apps)
+
+	geneticAlgorithm := algorithms.NewGenetic(200, 5000, 0.7, 0.007, 200, algorithms.RandomFitSchedule, clouds, apps)
+
+	solution, err := geneticAlgorithm.Schedule(clouds, apps)
+	if err != nil {
+		log.Panicf("geneticAlgorithm.Schedule(clouds, apps), error: %s", err.Error())
+	}
+
+	//log.Println()
+	if len(geneticAlgorithm.FitnessRecordBestUntilNow) != len(geneticAlgorithm.BestUntilNowUpdateIterations) {
+		log.Panicf("len(geneticAlgorithm.FitnessRecordBestUntilNow): %d, len(geneticAlgorithm.BestUntilNowUpdateIterations): %d\n", len(geneticAlgorithm.FitnessRecordBestUntilNow), len(geneticAlgorithm.BestUntilNowUpdateIterations))
+	}
+
+	i := len(geneticAlgorithm.FitnessRecordBestAcceptableUntilNow) - 1
+	//log.Printf("Iteration %d: FitnessRecordBestAcceptableUntilNow: %f\n", int(geneticAlgorithm.BestAcceptableUntilNowUpdateIterations[i]), geneticAlgorithm.FitnessRecordBestAcceptableUntilNow[i])
+	//
+	log.Println("solution:", solution)
+	log.Println(geneticAlgorithm.FitnessRecordBestAcceptableUntilNow[i])
+
+	assert.Equal(t, oriClouds, clouds, fmt.Sprintf("oriClouds and clouds are not equal"))
+	assert.Equal(t, oriApps, apps, fmt.Sprintf("oriApps and apps are not equal"))
+
+	fmt.Println(reflect.DeepEqual(oriClouds, clouds))
+	fmt.Println(reflect.DeepEqual(oriApps, apps))
+}
+
+func TestNoChangeFirstFit(t *testing.T) {
+	var numCloud, numApp int = 5, 15
+	var appSuffix string = "0"
+
+	// generate clouds and apps, and write to files
+	GenerateClouds(numCloud)
+	GenerateApps(numApp, appSuffix)
+
+	// read clouds and apps from files
+	var clouds []model.Cloud
+	var apps []model.Application
+
+	clouds = ReadClouds(numCloud)
+	apps = ReadApps(numApp, appSuffix)
+	clouds = model.CloudsCopy(clouds)
+	apps = model.AppsCopy(apps)
+
+	oriClouds := model.CloudsCopy(clouds)
+	oriApps := model.AppsCopy(apps)
+
+	ff := algorithms.NewFirstFit(clouds, apps)
+	solution, err := ff.Schedule(clouds, apps)
+	if err != nil {
+		log.Panicf("solution, err := ff.Schedule(oriClouds, oriApps), error: %s", err.Error())
+	}
+
+	log.Println("solution:", solution)
+
+	assert.Equal(t, oriClouds, clouds, fmt.Sprintf("oriClouds and clouds are not equal"))
+	assert.Equal(t, oriApps, apps, fmt.Sprintf("oriApps and apps are not equal"))
+
+	fmt.Println(reflect.DeepEqual(oriClouds, clouds))
+	fmt.Println(reflect.DeepEqual(oriApps, apps))
+}
+
+func TestNoChangeRandomFit(t *testing.T) {
+	var numCloud, numApp int = 5, 15
+	var appSuffix string = "0"
+
+	// generate clouds and apps, and write to files
+	GenerateClouds(numCloud)
+	GenerateApps(numApp, appSuffix)
+
+	// read clouds and apps from files
+	var clouds []model.Cloud
+	var apps []model.Application
+
+	clouds = ReadClouds(numCloud)
+	apps = ReadApps(numApp, appSuffix)
+	clouds = model.CloudsCopy(clouds)
+	apps = model.AppsCopy(apps)
+
+	oriClouds := model.CloudsCopy(clouds)
+	oriApps := model.AppsCopy(apps)
+
+	rf := algorithms.NewRandomFit(clouds, apps)
+	solution, err := rf.Schedule(clouds, apps)
+	if err != nil {
+		log.Panicf("solution, err := rf.Schedule(clouds, apps), error: %s", err.Error())
+	}
+
+	log.Println("solution:", solution)
+
+	assert.Equal(t, oriClouds, clouds, fmt.Sprintf("oriClouds and clouds are not equal"))
+	assert.Equal(t, oriApps, apps, fmt.Sprintf("oriApps and apps are not equal"))
+
+	fmt.Println(reflect.DeepEqual(oriClouds, clouds))
+	fmt.Println(reflect.DeepEqual(oriApps, apps))
 }
